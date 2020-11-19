5f4d8cf073abe4e45fca72a5078ab65ccfb784e6
giovannitf <Gi030201>
2020-11-19 00:10:11 -0300

    Inicia criação login

---
+++ app/src/main/java/com/br/newspaperapp/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.br.newspaperapp.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.br.newspaperapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
