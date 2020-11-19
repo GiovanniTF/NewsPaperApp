99514a6d35304b3be422bcae1157a59863389714
giovannitf <Gi030201>
2020-11-19 00:44:07 -0300

    Inicia Firebase Autenticação

--- app/src/main/java/com/br/newspaperapp/login/LoginActivity.kt
+++ app/src/main/java/com/br/newspaperapp/login/LoginActivity.kt
@@ -3,10 +3,24 @@ package com.br.newspaperapp.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.br.newspaperapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+//        updateUI()
     }
 }
