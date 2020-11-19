b084b2b53bdf227f633454006273132087b7972b
giovannitf <Gi030201>
2020-11-19 01:04:46 -0300

    Corrige SplashScreen e adiciona arquivo dimens

--- app/src/main/java/com/br/newspaperapp/splashScreen/SplashScreenActivity.kt
+++ app/src/main/java/com/br/newspaperapp/splashScreen/SplashScreenActivity.kt
@@ -10,16 +10,13 @@ import com.br.newspaperapp.R
 class SplashScreenActivity : AppCompatActivity() {
     private val SPLASH_TIME_OUT: Long = 3000
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed(
             {
-
                 startActivity(Intent(this, MainActivity::class.java))
-
                 finish()
             },
             SPLASH_TIME_OUT
